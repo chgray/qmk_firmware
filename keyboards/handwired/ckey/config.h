7c73fea727fef470a66de3aad3226c03922f960d
Chris Gray <chgray@gmail.com>
2021-04-10 10:31:14 -0700

    working!

--- keyboards/handwired/ckey/config.h
+++ keyboards/handwired/ckey/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT         CMD60
 
 /* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 14
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 4
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { F0, F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { B0, B1, B2, B3, B7, D0, D1, D2, D3, C6, D7, B4, B5, B6 }
+#define MATRIX_ROW_PINS { F0, F4, F5, F6, F7, B7}
+#define MATRIX_COL_PINS { B0, B1, B2, B3 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
