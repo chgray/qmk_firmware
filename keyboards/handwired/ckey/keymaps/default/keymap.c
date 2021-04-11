0b37e0c05eea053e97b99351562d8b9fc23e5b39
Chris Gray <chgray@gmail.com>
2021-04-10 21:13:14 -0700

    key maps

--- keyboards/handwired/ckey/keymaps/default/keymap.c
+++ keyboards/handwired/ckey/keymaps/default/keymap.c
@@ -2,12 +2,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* CMD60 - QWERTY */
-      KC_1,    KC_2,    KC_3,    KC_4, \
-      KC_5,    KC_6,    KC_7,    KC_8, \
-      KC_9,    KC_A,    KC_B,    KC_C, \
-      KC_D,    KC_E,    KC_F,    KC_G, \
-      KC_H,    KC_I,    KC_J,    KC_K, \
-      KC_L,    KC_M,    KC_N,    KC_O \
+      KC_ESC,    KC_9,    KC_0,    KC_L, \
+      KC_6,      KC_7,    KC_8,    KC_R, \
+      KC_2,      KC_3,    KC_4,    KC_5, \
+      KC_Q,      KC_W,    KC_E,    KC_1, \
+      KC_A,      KC_S,    KC_D,    KC_ENT, \
+      KC_LSFT,   KC_SLEP,    KC_VOLD,  KC_VOLU \
   ),
   [1] = LAYOUT( /* CMD60 - GameMode */
       KC_ESC,  KC_1,    KC_2,    KC_3, \
