070108a8b77c8b4c9aa4a01e93ab4356eae9c08d
Chris Gray <chgray@microsoft.com>
2021-09-22 11:29:54 -0700

    update vid/pid

--- keyboards/log0/yeet30/config.h
+++ keyboards/log0/yeet30/config.h
@@ -22,10 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4c47
-#define PRODUCT_ID      0x4447
+#define PRODUCT_ID      0x4347
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    LG
-#define PRODUCT         LG01
+#define PRODUCT         YEET3
 
 /* key matrix size */
 #define MATRIX_ROWS 7
